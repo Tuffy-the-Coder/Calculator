3b0156e75ec3cd96afdd8938e699786a33d934d3
Anurag <Rawat123anurag@gmail.com>
2025-04-21 22:08:48 +0530

    added display feature for digits and points

--- script.js
+++ script.js
@@ -56,177 +56,45 @@ document.body.addEventListener("keydown", (e) => {
 let num1 = "";
 let num2 = "";
 const operations = ["÷", "-", "×", "+"];
+const numbers = [1,2,3,4,5,6,7,8,9,0]
 let display = document.querySelector(".display");
 let currentOperation;
-// displaying digits
-
 document.querySelectorAll(".digit").forEach((digit) => {
-    digit.addEventListener("click", () => {
-        if (display.innerText.length >= 9) {
-            alert("max value exceed");
-            return;
-        }
-        else {
-            if (digit.innerText == ".") {
-                if (!operations.some(op => display.innerText.includes(op))) {
-                    if (!num1.includes(".")) {
-                        num1 += ".";
-                        display.innerText += ".";
-                    }
-                }
-                else {
-                    if (display.innerText[0].includes("-")) {
-                        num1 += ".";
-                        display.innerText += ".";
-                    }
-                    else {
-                        if (!num2.includes(".")) {
-                            num2 += ".";
-                            display.innerText += ".";
-                        }
-                    }
-                }
-            }
-            else {
-                if (!operations.some(op => display.innerText.includes(op))) {
-                    num1 += digit.innerText;
-                }
-                else {
-                    if (display.innerText[0].includes("-")) {
-                        num1 += digit.innerText;
-                    }
-                    else {
-                        num2 += digit.innerText;
-                    }
-                }
-                display.innerText += digit.innerText;
-            }
-        }
-
+    digit.addEventListener("click", (e) => {
+        displayDigit(e,digit.innerText);
+        // console.log(digit.innerText); // debugging console
     })
-
+})
+document.body.addEventListener("keydown", (e) => {
+    displayDigit(e,e.key);
+    // console.log(e.key); // debugging console
 })
 
-// displaying operators
-
-document.querySelectorAll(".operator").forEach((operator) => {
-    operator.addEventListener("click", () => {
-        if (display.innerText.length !== 0) {
-            if (!operations.some(op => display.innerText.includes(op))) {
-                displayingOperators(operator);
+const displayDigit = (e,digit) => {
+    if (display.innerText.length >= 9) {
+        alert("max value exceed");
+        return;
+    } else {
+        let hasOperator = operations.some(op => display.innerText.slice(1).includes(op)); // for placing value on num1 & num2
+        if(!hasOperator){
+            if (digit == ".") {
+                if (!num1.includes(".")) {  // to avoid dublicate . in num1
+                    num1 += digit;
+                    display.innerText += digit;
+                }
             } else {
-                if (display.innerText[0] == "-") {
-                    displayingOperators(operator);
-                }
+                num1 += digit;
+                display.innerText +=digit;
             }
-            if (operator.id == "equals-to") {
-                operation(currentOperation)
+        } else {
+            if (!num2.includes(".")) {     // to avoid dublicate . in num2
+                num2 +=digit;
+                display.innerText += digit;
+            } else {
+                num2 +=digit;
+                display.innerText += digit;
             }
-            else if (operator.id == "x-squared") {
-                operation("x^2");
-            }
-            else if (operator.id == "backspace") {
-                backspace();
-            }
-            else if (operator.id == "clear") {
-                display.innerText = "";
-                num1 = "";
-                num2 = "";
-            }
-            else if (operator.id == "plus-minus") {
-                if (display.innerText[0] == "-") {
-                    display.innerText = display.innerText.slice(1);
-                    num1 = num1.slice(1);
-                } else {
-                    display.innerText = "-".concat(display.innerText);
-                    num1 = "-".concat(num1);
-                }
-            }
-        }
-
-    })
-})
-function displayingOperators(operator) {
-    if (operator.id == "add") {
-        display.innerText += "+";
-        currentOperation = "+";
-    }
-    else if (operator.id == "substract") {
-        display.innerText += "-";
-        currentOperation = "-";
-    }
-    else if (operator.id == "multiply") {
-        display.innerText += "×";
-        currentOperation = "×";
-    }
-    else if (operator.id == "divide") {
-        display.innerText += "÷";
-        currentOperation = "÷";
-    }
-
-}
-// backspace 
-
-function backspace() {
-    console.log("Backspace button clicked!"); 
-    display.innerText = display.innerText.slice(0, -1);
-    if (!operations.some(op => display.innerText.includes(op))) {
-        num1 = num1.slice(0, -1);
-    }
-    else {
-        if (display.innerText[0].includes("-")) {
-            num1 = num1.slice(0, -1);
-        }
-        else {
-            num2 = num2.slice(0, -1);
+            
         }
     }
-}
-
-
-
-document.addEventListener("keydown", (e) => {
-    if (e.key == "Backspace") {
-        backspace();
-    }
-})
-
-// operation
-function operation(currentOperation) {
-
-    switch (currentOperation) {
-        case "+":
-            ans = Number(num1) + Number(num2);
-            break;
-        case "-":
-            ans = Number(num1) - Number(num2);
-            break;
-        case "×":
-            ans = Number(num1) * Number(num2);
-            break;
-        case "÷":
-            ans = Number(num1) / Number(num2);
-            break;
-        case "x^2":
-            ans = Number(num1) * Number(num1);
-            if (ans>=99999999) {
-                alert("max value exceed");
-                return;
-            }
-            break;
-        default:
-            display.innerText = display.innerText;
-            break;
-    }
-    if (ans % 1 != 0) {
-        display.innerText = ans.toFixed(2);
-        num1 = ans.toFixed(2);
-        num2 = ""
-    }
-    else {
-        display.innerText = ans;
-        num1 = ans.toString();
-        num2 = "";
-    }
-
 }
